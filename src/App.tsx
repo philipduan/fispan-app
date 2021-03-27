f43b9534d3b3ccf66a5a0eb6b14f7f2fa2da4101
Philip Du <philip_du_an@hotmail.com>
2021-03-27 16:10:02 -0700

    Complete initial show rate, median rate and buy/sell suggestion

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { message } from "antd";
+import {
+  getLatestRate,
+  getPastThirtyDaysRate,
+  getMedianFromTimeSeries,
+} from "./utils/utilities";
+import "./App.scss";
 
 function App() {
+  const [rate, setRate] = useState<number>(0);
+  const [rateMedian, setRateMedian] = useState<number>(0);
+
+  useEffect(() => {
+    getLatestRate("cad", "usd")
+      .then((data) => {
+        setRate(data.rates.USD);
+      })
+      .catch(({ message: errorMessage }) => {
+        message.error(errorMessage);
+      });
+
+    getPastThirtyDaysRate("cad", "usd")
+      .then((data) => {
+        setRateMedian(getMedianFromTimeSeries(data.rates));
+      })
+      .catch(({ message: errorMessage }) => {
+        message.error(errorMessage);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>1 CAD is {rate} to USD</p>
+      <p>The rate median of CAD to USD for the last 30 days is {rateMedian}</p>
+      <p>
+        It is best to{" "}
+        {rate < rateMedian ? "buy" : rate > rateMedian ? "sell" : "do nothing"}
+      </p>
     </div>
   );
 }
