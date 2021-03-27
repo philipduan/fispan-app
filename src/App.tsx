0e635237adba431b5fe145bd233f6886d2f29443
Philip Du <philip_du_an@hotmail.com>
2021-03-27 16:42:42 -0700

    Add buy sell switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { message } from "antd";
+import { message, Switch } from "antd";
 import {
   getLatestRate,
   getPastThirtyDaysRate,
   getMedianFromTimeSeries,
 } from "./utils/utilities";
 import "./App.scss";
+import "antd/dist/antd.css";
 
-function App() {
+const App = () => {
   const [rate, setRate] = useState<number>(0);
   const [rateMedian, setRateMedian] = useState<number>(0);
+  const [isBuy, setIsBuy] = useState<boolean>(true);
 
   useEffect(() => {
     getLatestRate("cad", "usd")
@@ -29,16 +31,33 @@ function App() {
       });
   }, []);
 
+  const buySellToggleChange = () => {
+    setIsBuy((isPrevIsBuy) => !isPrevIsBuy);
+  };
+
+  const goodOrNot = () => {
+    if (rate < rateMedian) {
+      return isBuy ? "good" : "not good";
+    } else {
+      return isBuy ? "not good" : "good";
+    }
+  };
+
   return (
     <div className="App">
       <p>1 CAD is {rate} to USD</p>
       <p>The rate median of CAD to USD for the last 30 days is {rateMedian}</p>
+      <Switch
+        defaultChecked={isBuy}
+        checkedChildren="Buy"
+        unCheckedChildren="Sell"
+        onChange={buySellToggleChange}
+      />
       <p>
-        It is best to{" "}
-        {rate < rateMedian ? "buy" : rate > rateMedian ? "sell" : "do nothing"}
+        It's {goodOrNot()} to {isBuy ? "buy" : "sell"}
       </p>
     </div>
   );
-}
+};
 
 export default App;
