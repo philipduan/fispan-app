f38d627b4853e8bb6ce0b25b50b9d604aa8e8e7c
Philip Du <philip_du_an@hotmail.com>
2021-03-28 16:45:29 -0700

    Add charts, to and from currency and dropdown selections

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,74 @@
 import React, { useState, useEffect } from "react";
-import { Layout, message, Space, Switch, Typography } from "antd";
+import moment from "moment";
+import { Layout, message, Select, Space, Switch, Typography } from "antd";
 import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  MarkSeries,
+} from "react-vis";
+import {
+  getCurrencies,
   getLatestRate,
   getPastThirtyDaysRate,
   getMedianFromTimeSeries,
 } from "./utils/utilities";
-import "./App.scss";
+import "react-vis/dist/style.css";
 import "antd/dist/antd.css";
+import "./App.scss";
 
-const { Header, Footer, Content } = Layout;
+const { Header, Content } = Layout;
 const { Title, Paragraph } = Typography;
+const { Option } = Select;
 
 const App = () => {
+  const [currencyList, setCurrencyList] = useState<string[]>([]);
+  const [fromCurrency, setFromCurrency] = useState<string>("CAD");
+  const [toCurrency, setToCurrency] = useState<string>("USD");
   const [rate, setRate] = useState<number>(0);
   const [rateMedian, setRateMedian] = useState<number>(0);
   const [isBuy, setIsBuy] = useState<boolean>(true);
+  const [chartData, setChartData] = useState<any>([]);
 
   useEffect(() => {
-    getLatestRate("cad", "usd")
+    getCurrencies()
       .then((data) => {
-        setRate(data.rates.USD);
+        setCurrencyList(Object.keys(data));
       })
       .catch(({ message: errorMessage }) => {
-        message.error(errorMessage);
-      });
-
-    getPastThirtyDaysRate("cad", "usd")
-      .then((data) => {
-        setRateMedian(getMedianFromTimeSeries(data.rates));
-      })
-      .catch(({ message: errorMessage }) => {
-        message.error(errorMessage);
+        showErrorMessage(errorMessage);
       });
   }, []);
+
+  useEffect(() => {
+    getLatestRate(fromCurrency, toCurrency)
+      .then((data) => {
+        setRate(data.rates[toCurrency]);
+      })
+      .catch(({ message: errorMessage }) => {
+        showErrorMessage(errorMessage);
+      });
+
+    getPastThirtyDaysRate(fromCurrency, toCurrency)
+      .then((data) => {
+        setChartData(
+          Object.entries(data.rates).map(([key, value]) => ({
+            x: new Date(key),
+            y: value[toCurrency],
+          }))
+        );
+        setRateMedian(getMedianFromTimeSeries(toCurrency, data.rates));
+      })
+      .catch(({ message: errorMessage }) => {
+        showErrorMessage(errorMessage);
+      });
+  }, [fromCurrency, toCurrency]);
+
+  const showErrorMessage = (errorMessage: string) => {
+    message.error(errorMessage);
+  };
 
   const buySellToggleChange = () => {
     setIsBuy((isPrevIsBuy) => !isPrevIsBuy);
@@ -58,9 +94,35 @@ const App = () => {
           style={{ padding: 10, display: "flex", justifyContent: "center" }}
         >
           <Space direction="vertical" align="center">
-            <Paragraph>1 CAD is {rate} USD</Paragraph>
             <Paragraph>
-              The rate median of CAD to USD for the last 30 days is {rateMedian}
+              1
+              <Select
+                style={{ margin: 10 }}
+                defaultValue={fromCurrency}
+                onChange={(value) => {
+                  setFromCurrency(value);
+                }}
+              >
+                {currencyList.map((currency) => (
+                  <Option value={currency}>{currency}</Option>
+                ))}
+              </Select>
+              is {rate}
+              <Select
+                style={{ margin: 10 }}
+                defaultValue={toCurrency}
+                onChange={(value) => {
+                  setToCurrency(value);
+                }}
+              >
+                {currencyList.map((currency) => (
+                  <Option value={currency}>{currency}</Option>
+                ))}
+              </Select>
+            </Paragraph>
+            <Paragraph>
+              The rate median of {fromCurrency} to {toCurrency} for the last 30
+              days is {rateMedian}
             </Paragraph>
             <Switch
               defaultChecked={isBuy}
@@ -72,6 +134,26 @@ const App = () => {
             <Paragraph>
               It's {goodOrNot()} to {isBuy ? "buy" : "sell"}
             </Paragraph>
+            {chartData.length ? (
+              <XYPlot
+                xType="time"
+                width={1000}
+                height={500}
+                margin={{ left: 60, right: 10, top: 10, bottom: 100 }}
+              >
+                <VerticalGridLines />
+                <HorizontalGridLines />
+                <XAxis
+                  title="Date"
+                  tickLabelAngle={-45}
+                  tickFormat={(tick) => moment(tick).format("DD-MM-YYYY")}
+                />
+                <YAxis title="Rate" tickLabelAngle={-45} />
+                <MarkSeries data={chartData} strokeWidth={2} />
+              </XYPlot>
+            ) : (
+              <></>
+            )}
           </Space>
         </Content>
       </Layout>
--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,39 +1,62 @@
-import axios from 'axios';
-import moment from 'moment'
-import { LatestResponse, Rates, TimeSeriesResponse } from './interface';
+import axios from "axios";
+import moment from "moment";
+import { LatestResponse, Rates, TimeSeriesResponse } from "./interface";
 
-export const baseUrl = 'https://api.frankfurter.app';
+export const baseUrl = "https://api.frankfurter.app";
 
-export const getLatestRate = async (fromCurrency: string, toCurrency: string): Promise<LatestResponse>  => {
+export const getCurrencies = async (): Promise<Record<string, string>> => {
   try {
-    const lastestResponse = await axios.get<LatestResponse>(`${baseUrl}/latest?from=${fromCurrency}&to=${toCurrency}`);
+    const currenciesResponse = await axios.get(`${baseUrl}/currencies`);
+    return currenciesResponse.data;
+  } catch (e) {
+    throw new Error("Cannot get currencies");
+  }
+};
+export const getLatestRate = async (
+  fromCurrency: string,
+  toCurrency: string
+): Promise<LatestResponse> => {
+  try {
+    const lastestResponse = await axios.get<LatestResponse>(
+      `${baseUrl}/latest?from=${fromCurrency}&to=${toCurrency}`
+    );
     return lastestResponse.data;
-  } catch(error) {
-    throw new Error('Cannot get latest rate')
+  } catch (error) {
+    throw new Error("Cannot get latest rate");
   }
-}
+};
 
-export const getPastThirtyDaysRate = async (fromCurrency: string, toCurrency: string): Promise<TimeSeriesResponse> => {
+export const getPastThirtyDaysRate = async (
+  fromCurrency: string,
+  toCurrency: string
+): Promise<TimeSeriesResponse> => {
   try {
-    const dateToday = moment().format("yyyy-MM-DD")
-    const dateThirtyDaysBefore = moment().subtract(30, "days").format("yyy-MM-DD");
-    const pastThirtyDaysRateResponse =  await axios.get(`${baseUrl}/${dateThirtyDaysBefore}..${dateToday}?from=${fromCurrency}&to=${toCurrency}`);
+    const dateToday = moment().format("yyyy-MM-DD");
+    const dateThirtyDaysBefore = moment()
+      .subtract(30, "days")
+      .format("yyyy-MM-DD");
+    const pastThirtyDaysRateResponse = await axios.get(
+      `${baseUrl}/${dateThirtyDaysBefore}..${dateToday}?from=${fromCurrency}&to=${toCurrency}`
+    );
     return pastThirtyDaysRateResponse.data;
-  } catch(error) {
-    throw new Error('Cannot get past 30 days rate')
+  } catch (error) {
+    throw new Error("Cannot get past 30 days rate");
   }
-}
+};
 
-export const getMedianFromTimeSeries = (rates: Record<string, Rates>): number => {
+export const getMedianFromTimeSeries = (
+  toCurrency: string,
+  rates: Record<string, Rates>
+): number => {
   let medianRate: number = 0;
   const ratesArray = Object.values(rates)
-        .map((rate) => rate.USD)
-        .sort();
+    .map((rate) => rate[toCurrency])
+    .sort();
   const middleArray = Math.floor(ratesArray.length / 2);
   if (middleArray % 2 === 0) {
     medianRate = (ratesArray[middleArray - 1] + ratesArray[middleArray]) / 2;
   } else {
-    medianRate = (ratesArray[middleArray]);
+    medianRate = ratesArray[middleArray];
   }
-  return medianRate;
-}
+  return +medianRate.toFixed(4);
+};
